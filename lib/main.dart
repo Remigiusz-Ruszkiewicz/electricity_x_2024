ad3036ff2e758a29d2d7d4dc6ef27d0f3bb42f24
Remigiusz-Ruszkiewicz <RemikR2000@interia.pl>
2022-12-16 19:56:13 +0100

    languages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 //import 'package:samalert/API_bloc.dart';
 import 'package:electricity_x_2024/login_view.dart';
 import 'package:electricity_x_2024/src/resources/themes/dark/dark_theme.dart';
@@ -29,9 +27,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print(message.messageId);
-    });
     return MaterialApp(
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,

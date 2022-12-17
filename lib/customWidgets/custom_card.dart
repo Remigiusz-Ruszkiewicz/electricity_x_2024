33f918ce4a1b68d640c9d9bd84086bec6d4e597c
k_domon <k.domon@dasl.pl>
2022-12-17 04:26:10 +0100

    giver panel

--- lib/customWidgets/custom_card.dart
+++ lib/customWidgets/custom_card.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
-  const CustomCard({required this.color, required this.content, Key? key}) : super(key: key);
+  const CustomCard({
+    this.color = const Color.fromRGBO(100, 255, 110, .3),
+    required this.content,
+    Key? key,
+  }) : super(key: key);
 
   final Color color;
   final Widget content;
@@ -19,4 +23,25 @@ class CustomCard extends StatelessWidget {
       child: content,
     );
   }
+}
+
+Widget getCard(String text) {
+  return CustomCard(
+    content: SizedBox(
+      height: 100,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Text(
+            text,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
--- lib/giver_panel.dart
+++ lib/giver_panel.dart
@@ -1,8 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
-import 'package:electricity_x_2024/customWidgets/custom_card.dart';
 import 'package:flutter/material.dart';
 
 import 'base/page_widget.dart';
+import 'customWidgets/custom_card.dart';
 import 'generated/locale_keys.g.dart';
 
 class GiverPanel extends StatelessWidget {
@@ -17,39 +17,17 @@ class GiverPanel extends StatelessWidget {
         width: 300,
         height: 200,
       ),
-      midWidget: _getCard('W tym tygodniu wyprodukowałeś 23232kWh eko energii!'),
+      midWidget: getCard('W tym tygodniu wyprodukowałeś 23232kWh eko energii!'),
       bottomWidget: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Expanded(
-            child: _getCard('Odsprzedałeś 456kWh'),
+            child: getCard('Odsprzedałeś 456kWh'),
           ),
           Expanded(
-            child: _getCard('Zarobiłeś 1111zł'),
+            child: getCard('Zarobiłeś 1111zł'),
           ),
         ],
-      ),
-    );
-  }
-
-  Widget _getCard(String text) {
-    return CustomCard(
-      color: Colors.green,
-      content: SizedBox(
-        height: 100,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Text(
-              text,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-              ),
-            ),
-          ),
-        ),
       ),
     );
   }

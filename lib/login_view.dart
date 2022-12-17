dd3cd1c5e5dd6f62ef50e2feed668b8f13b238f5
Remigiusz-Ruszkiewicz <RemikR2000@interia.pl>
2022-12-17 07:02:16 +0100

    new views

--- lib/login_view.dart
+++ lib/login_view.dart
@@ -22,121 +22,92 @@ class _LoginViewState extends State<LoginView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: null,
-      body: GestureDetector(
-        onTap: () {
-          FocusScope.of(context).requestFocus(FocusNode());
-        },
-        child: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(35.0),
-            child: SingleChildScrollView(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+              alignment: Alignment.topCenter,
+              image: AssetImage("assets/images/logo.png"),
+              fit: BoxFit.cover,
+              opacity: 0.1),
+        ),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(FocusNode());
+          },
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(35.0),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(
-                        height: 120,
-                        width: 120,
-                        child: Card(
-                          semanticContainer: true,
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                          elevation: 5,
-                          color: Colors.grey,
-                          margin: const EdgeInsets.all(10),
-                          child: const Image(image: AssetImage('assets/images/ratmon_icon.png')),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const Padding(padding: EdgeInsets.fromLTRB(0, 10, 0, 10)),
+                  SizedBox(height: 50,),
                   Text(
-                    LocaleKeys.LoginTitle.tr(),
+                    'TU BĘDZIE NAZWA',
                     style: const TextStyle(color: Colors.white, fontSize: 30),
                     textAlign: TextAlign.center,
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 30, bottom: 15),
-                        child: Text(
-                          LocaleKeys.Email.tr(),
-                          style: const TextStyle(
-                            color: Color.fromRGBO(186, 186, 186, 1),
-                            fontSize: 13,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  SizedBox(height: 250,),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 15),
                     child: TextFormField(
-                      decoration: const InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              width: 2,
-                              color: Color.fromRGBO(54, 53, 53, 1),
-                            ),
-                          ),
-                          constraints: BoxConstraints(maxWidth: 400)),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
-                        child: Text(
-                          LocaleKeys.Password.tr(),
-                          style: const TextStyle(
-                            color: Color.fromRGBO(186, 186, 186, 1),
-                            fontSize: 13,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 15),
-                    child: TextFormField(
-                      obscureText: !_passwordVisible,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      obscuringCharacter: "*",
                       decoration: InputDecoration(
-                        enabledBorder: const OutlineInputBorder(
-                          borderSide: BorderSide(
-                            width: 2,
-                            color: Color.fromRGBO(54, 53, 53, 1),
-                          ),
-                        ),
-                        constraints: const BoxConstraints(maxWidth: 400),
-                        suffixIcon: IconButton(
-                          onPressed: () => {
-                            setState(
-                              () {
-                                _passwordVisible = !_passwordVisible;
-                              },
+                          filled: true,
+                          fillColor: Colors.black12.withOpacity(0.1),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Colors.green, //.fromRGBO(54, 53, 53, 1),
                             ),
-                          },
-                          color: Colors.blue,
-                          icon: const Icon(Icons.remove_red_eye_outlined),
+                          ),
+                          hintText: 'E-Mail',
+                          prefixIcon: Icon(Icons.person),
+                          constraints: const BoxConstraints(maxWidth: 400)),
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        left: 15.0, right: 15.0, top: 15, bottom: 15),
+                    child: Container(
+                      child: TextFormField(
+                        obscureText: !_passwordVisible,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        obscuringCharacter: "*",
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.black12.withOpacity(0.1),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              width: 1,
+                              color: Colors.green,
+                            ),
+                          ),
+                          hintText: 'Hasło',
+                          prefixIcon: Icon(Icons.lock),
+                          constraints: const BoxConstraints(maxWidth: 400),
+                          suffixIcon: IconButton(
+                            onPressed: () => {
+                              setState(
+                                () {
+                                  _passwordVisible = !_passwordVisible;
+                                },
+                              ),
+                            },
+                            color: Colors.green,
+                            icon: const Icon(Icons.remove_red_eye_outlined),
+                          ),
                         ),
                       ),
                     ),
                   ),
+                  SizedBox(height: 100,),
                   Container(
                     height: 50,
                     width: 250,
                     decoration: BoxDecoration(
-                      color: Colors.blue,
+                      color: Colors.green,
                       borderRadius: BorderRadius.circular(4),
                     ),
                     child: MaterialButton(
@@ -148,7 +119,8 @@ class _LoginViewState extends State<LoginView> {
                       ),
                       child: Text(
                         LocaleKeys.Login.tr(),
-                        style: const TextStyle(color: Colors.white, fontSize: 16),
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 16),
                       ),
                     ),
                   ),
@@ -157,17 +129,23 @@ class _LoginViewState extends State<LoginView> {
                     height: 50,
                     width: 250,
                     decoration: BoxDecoration(
-                      border: Border.all(width: 2, color: const Color.fromRGBO(117, 117, 117, 1)),
+                      border: Border.all(
+                          width: 2,
+                          color: const Color.fromRGBO(117, 117, 117, 1)),
                       color: Colors.transparent,
                       borderRadius: BorderRadius.circular(4),
                     ),
                     child: MaterialButton(
                       onPressed: () {
-                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const RegisterView()));
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (_) => const RegisterView()));
                       },
                       child: Text(
                         LocaleKeys.Register.tr(),
-                        style: const TextStyle(color: Colors.white, fontSize: 16),
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 16),
                       ),
                     ),
                   ),
